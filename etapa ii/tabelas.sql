573b22dcb0a2c9963a8f83446277283c5bc8255c
Stefany Nascimento <stefanycn.dev@gmail.com>
2025-06-19 17:20:26 -0300

    Fix unique tuple references and remove unused view

--- etapa ii/tabelas.sql
+++ etapa ii/tabelas.sql
@@ -1,19 +1,7 @@
--- CRIAÇÃO DE VISÕES
--- Essa visão agrega informações sobre publicações, incluindo o número total de visualizações, comentários e
--- curtidas, permitindo consultas mais eficientes sobre o desempenho das publicações.  
-CREATE OR REPLACE VIEW INFO_PUBLICACOES AS
-SELECT p.id_publicacao, p.data_publicacao, p.descricao,
-       COUNT(DISTINCT v.id_conta) AS total_visualizacoes,
-       COUNT(DISTINCT c.id_comentario) AS total_comentarios,
-       COUNT(DISTINCT curt.id_curtida) AS total_curtidas
-FROM PUBLICACOES p
-LEFT JOIN VISUALIZACOES v ON p.id_publicacao = v.id_publicacao
-LEFT JOIN COMENTARIOS c ON p.id_publicacao = c.id_publicacao
-LEFT JOIN CURTIDAS curt ON p.id_publicacao = curt.id_publicacao
-GROUP BY p.id_publicacao, p.data_publicacao, p.descricao;
 
-
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 -- CRIAÇÃO DAS TABELAS DA APLICAÇÃO INSTAGRADO
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 -- OBS: 
 --     - Foi usado SERIAL para as id's para gerar automaticamente
 --       id's incrementadas
@@ -34,7 +22,7 @@ CREATE TABLE CRIADORES_DE_CONTEUDO (
     id_conta INT UNIQUE NOT NULL,
     interacao INT,  
     visualizacoes_do_perfil INT,                               
-    FOREIGN KEY (id_conta) REFERENCES CONTA(id_conta)
+    FOREIGN KEY (id_conta) REFERENCES CONTAS(id_conta)
 );
 
 -- Tabela: PERFIS
@@ -42,7 +30,7 @@ CREATE TABLE PERFIS (
     id_perfil SERIAL PRIMARY KEY,
     id_conta INT UNIQUE NOT NULL,
     descricao TEXT,
-    FOREIGN KEY (id_conta) REFERENCES CONTA(id_conta)
+    FOREIGN KEY (id_conta) REFERENCES CONTAS(id_conta)
 );
 
 -- Tabela: PUBLICACOES
@@ -59,7 +47,7 @@ CREATE TABLE POSTS (
     id_post SERIAL PRIMARY KEY,
     id_publicacao INT UNIQUE NOT NULL,
     privado BOOLEAN,
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao)
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao)
 );
 
 -- Tabela: STORIES
@@ -67,7 +55,7 @@ CREATE TABLE STORIES (
     id_story SERIAL PRIMARY KEY,
     id_publicacao INT UNIQUE NOT NULL,
     tempo_expiracao TIME,                               -- COMO GARANTIR AS 24 HORAS??
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao)
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao)
 );
 
 -- Tabela: REELS
@@ -75,7 +63,7 @@ CREATE TABLE REELS (
     id_reels SERIAL PRIMARY KEY,
     id_publicacao INT UNIQUE NOT NULL,
     duracao INT,
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao)
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao)
 );
 
 -- Tabela: MÍDIAS
@@ -86,8 +74,8 @@ CREATE TABLE MIDIAS (
     extensao VARCHAR(20),
     nome_conteudo VARCHAR(200),
     conteudo BYTEA NOT NULL,
-    (nome_conteudo,extensao) UNIQUE,                    -- NAO LEMBRO COMO FAZ ISSO
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao)
+    UNIQUE (nome_conteudo,extensao),                    -- NAO LEMBRO COMO FAZ ISSO
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao)
 );
 
 -- Tabela: MENSAGEM
@@ -97,8 +85,8 @@ CREATE TABLE MENSAGENS (
     id_destinatario INT NOT NULL,
     conteudo TEXT NOT NULL,
     data_msg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_remetente) REFERENCES CONTA(id_conta),
-    FOREIGN KEY (id_destinatario) REFERENCES CONTA(id_conta)
+    FOREIGN KEY (id_remetente) REFERENCES CONTAS(id_conta),
+    FOREIGN KEY (id_destinatario) REFERENCES CONTAS(id_conta)
 );
 
 -- Tabela: VISUALIZACOES
@@ -106,8 +94,8 @@ CREATE TABLE VISUALIZACOES (
     id_visualizacao SERIAL PRIMARY KEY,
     id_publicacao INT NOT NULL,
     id_conta INT NOT NULL,
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao),
-    FOREIGN KEY (id_conta) REFERENCES CONTA(id_conta)
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao),
+    FOREIGN KEY (id_conta) REFERENCES CONTAS(id_conta)
 );
 
 -- Tabela: CURTIDAS
@@ -115,8 +103,8 @@ CREATE TABLE CURTIDAS (
     id_curtida SERIAL PRIMARY KEY,
     id_publicacao INT NOT NULL,
     id_conta INT NOT NULL,
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao),
-    FOREIGN KEY (id_conta) REFERENCES CONTA(id_conta)
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao),
+    FOREIGN KEY (id_conta) REFERENCES CONTAS(id_conta)
 );
 
 -- Tabela: COMENTARIOS
@@ -126,6 +114,6 @@ CREATE TABLE COMENTARIOS (
     id_conta INT NOT NULL,
     conteudo TEXT NOT NULL,
     data_comentario TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACAO(id_publicacao),
-    FOREIGN KEY (id_conta) REFERENCES CONTA(id_conta)
+    FOREIGN KEY (id_publicacao) REFERENCES PUBLICACOES(id_publicacao),
+    FOREIGN KEY (id_conta) REFERENCES CONTAS(id_conta)
 );
